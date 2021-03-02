55f087b3fb79fb771d1ca0d6bb5261ef699aae57
arockia <arockiadossaa@gmail.com>
2021-03-02 22:47:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet'
+import Welcome from './components/Welcome'
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <Greet/>
+     <Welcome/>
+      <h1>Hi</h1>
+
+     
+    </div>
+     <h1>test pragraph</h1>
+     </>
+  );
+}
+
+export default App;
